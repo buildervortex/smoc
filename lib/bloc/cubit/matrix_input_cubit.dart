d8a85cb73b1bec22237ccc77b981bf28018d0bc8
galahirudilhara <galahirudilhara@gmail.com>
2025-04-27 16:18:25 +0530

    add the matrix input cubit and its state

--- lib/bloc/cubit/matrix_input_cubit.dart
+++ lib/bloc/cubit/matrix_input_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:smo/models/sparse_matrix.dart';
 
 part 'matrix_input_state.dart';
 
 class MatrixInputCubit extends Cubit<MatrixInputState> {
-  MatrixInputCubit() : super(MatrixInputInitial());
+  MatrixInputCubit()
+      : super(MatrixInputInitial(
+            sparseMatrix: SparseMatrix(columns: 0, rows: 0, entries: [])));
 }
--- lib/bloc/cubit/matrix_input_state.dart
+++ lib/bloc/cubit/matrix_input_state.dart
@@ -1,10 +1,24 @@
 part of 'matrix_input_cubit.dart';
 
 sealed class MatrixInputState extends Equatable {
-  const MatrixInputState();
+  final SparseMatrix sparseMatrix;
+
+  const MatrixInputState({required this.sparseMatrix});
 
   @override
   List<Object> get props => [];
 }
 
-final class MatrixInputInitial extends MatrixInputState {}
+final class MatrixInputInitial extends MatrixInputState {
+  const MatrixInputInitial({required super.sparseMatrix});
+}
+
+final class MatrixInputSuccess extends MatrixInputState {
+  const MatrixInputSuccess({required super.sparseMatrix});
+}
+
+final class MatrixInputFailed extends MatrixInputState {
+  final String errorMessage;
+  const MatrixInputFailed(
+      {required super.sparseMatrix, required this.errorMessage});
+}
