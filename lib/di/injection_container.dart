8b50f838ec0be9929fc691febb23be9ba05221e3
galahirudilhara <galahirudilhara@gmail.com>
2025-04-27 16:21:41 +0530

    add the cubits to dependency container

--- lib/di/injection_container.dart
+++ lib/di/injection_container.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:smo/bloc/cubit/matrix_input_cubit.dart';
+import 'package:smo/bloc/cubit/operation_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -6,4 +8,7 @@ Future<void> initDiContainer() async {
   // Register your services, repositories, and other dependencies here
   // Example:
   // sl.registerLazySingleton<SomeService>(() => SomeServiceImpl());
+
+  sl.registerFactory<MatrixInputCubit>(() => MatrixInputCubit());
+  sl.registerFactory<OperationCubit>(() => OperationCubit());
 }
