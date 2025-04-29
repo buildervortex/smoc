9c1074a806760d14973e6cbaecead752bc297d65
galahirudilhara <galahirudilhara@gmail.com>
2025-04-29 15:09:19 +0530

    add the adjoint usecase and fully tested'

---
+++ test/usecases/adjoint_usecase_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smo/usecases/adjoint_usecase.dart';
+
+void main() {
+  group('CheckSingularMatrixUsecase', () {
+    test('should return the adjoint of a 2x2 matrix', () {
+      final usecase = CheckSingularMatrixUsecase();
+      final matrix = {
+        0: {0: 1, 1: 2},
+        1: {0: 3, 1: 4},
+      };
+      final expectedAdjoint = {
+        0: {0: 4, 1: -2},
+        1: {0: -3, 1: 1},
+      };
+
+      final result = usecase(matrix, 2);
+
+      expect(result, expectedAdjoint);
+    });
+
+    test('should return the adjoint of a 3x3 matrix', () {
+      final usecase = CheckSingularMatrixUsecase();
+      final matrix = {
+        0: {0: 1, 1: 2, 2: 3},
+        1: {1: 1, 2: 4},
+        2: {0: 5, 1: 6},
+      };
+      final expectedAdjoint = {
+        0: {0: -24, 1: 18, 2: 5},
+        1: {0: 20, 1: -15, 2: -4},
+        2: {0: -5, 1: 4, 2: 1},
+      };
+
+      final result = usecase(matrix, 3);
+
+      expect(result, expectedAdjoint);
+    });
+
+    test('should handle an empty matrix', () {
+      final usecase = CheckSingularMatrixUsecase();
+      final matrix = <int, Map<int, num>>{};
+      final expectedAdjoint = <int, Map<int, num>>{};
+
+      final result = usecase(matrix, 0);
+
+      expect(result, expectedAdjoint);
+    });
+  });
+}
